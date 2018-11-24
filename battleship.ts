ef7185d862e388c8928a8ed307a454c716c8c6d8
BlueKraken <gonzalo.tajmuch@gmail.com>
2018-11-24 20:22:23 -0300

    Extrae función randomIntFromInterval a un archivo contenedor de funciones de utilidad

--- battleship.ts
+++ battleship.ts
@@ -1,6 +1,4 @@
-function randomIntFromInterval(min,max) {// min and max included
-    return Math.floor(Math.random()*(max-min+1)+min);
-}
+import { randomIntFromInterval } from './functions';
 
 class Board {
     rows:number;
