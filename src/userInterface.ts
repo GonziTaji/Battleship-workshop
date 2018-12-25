386ec2d3b334e98b97fe3437d27b4217cb94ed24
BlueKraken <gonzalo.tajmuch@gmail.com>
2018-12-25 12:43:23 -0300

    Desacopla UserInterface de las posiciones del juego (excepto para su creación)

--- src/userInterface.ts
+++ src/userInterface.ts
@@ -1,18 +1,15 @@
 import { BoardCell } from "./boardCell";
+import { gameConfig } from "./gameConfig";
 
 export class UserInterface {
     private positionsContainer: HTMLDivElement;
-    private boardPositions: BoardCell[][];
+    private boardPositions: HTMLDivElement[][];
     private selectedRow: number = 0;
     private selectedColumn: number = 0;
-    private shootHandler: (x: number, y: number) => any;
 
     constructor(positions: BoardCell[][], shotCallback: (x: number, y: number) => any) {
         this.positionsContainer = document.createElement('div');
         document.body.append(this.positionsContainer);
-              
-        this.shootHandler = shotCallback;
-        this.boardPositions = positions;
 
         this.renderBoard(positions, shotCallback);
         this.renderForm();
@@ -20,6 +17,7 @@ export class UserInterface {
 
     private renderBoard(positions: BoardCell[][], shotCallback: (x: number, y: number) => any) {
         this.positionsContainer.innerHTML = "";
+        this.boardPositions = [];
 
         positions.map((row, x) => 
             row.map((cell, y) => 
@@ -30,6 +28,7 @@ export class UserInterface {
             const rowContainer = document.createElement('div');
             rowContainer.style.height = row[0].style.height;
             rowContainer.append(...row);
+            this.boardPositions.push(row);
             this.positionsContainer.appendChild(rowContainer);
         });
     }   
@@ -37,18 +36,39 @@ export class UserInterface {
     private renderForm() {
         const rowSelectionInput = document.createElement('input');
         const columnselectionInput = document.createElement('input');
+        
+        rowSelectionInput.type = "number";
+        columnselectionInput.type = "number";
+        rowSelectionInput.max = gameConfig.board.rows.toString();
+        columnselectionInput.max = gameConfig.board.columns.toString();
 
-        rowSelectionInput.oninput = e => 
-            this.selectedRow = parseInt((e.target as HTMLInputElement).value);
+        rowSelectionInput.oninput = e => {
+            this.selectFromForm(true);
 
-        columnselectionInput.oninput = e => 
-            this.selectedRow = parseInt((e.target as HTMLInputElement).value);
+            const value = parseInt((e.target as HTMLInputElement).value);
+
+            if (!isNaN(value) && value < gameConfig.board.rows) {
+                this.selectedRow = value;
+                this.selectFromForm();
+            }
+        }
+
+        columnselectionInput.oninput = e => {
+            this.selectFromForm(true);
+
+            const value = parseInt((e.target as HTMLInputElement).value);
+            
+            if (!isNaN(value) && value < gameConfig.board.columns) {
+                this.selectedColumn = value;
+                this.selectFromForm();
+            }
+        }
 
         const shootButton = document.createElement('button');
         shootButton.innerText = "Dispara!"
         shootButton.onclick = () => {
-            this.shootHandler(this.selectedColumn, this.selectedRow);
-            this.renderBoard(this.boardPositions, this.shootHandler);
+            this.boardPositions[this.selectedColumn][this.selectedRow]
+                .click();
         }
 
         document.body.append(
@@ -57,6 +77,13 @@ export class UserInterface {
             'Y:', 
             rowSelectionInput,
             shootButton)
+    }
+
+    selectFromForm(deselect = false) {
+        const eventName = deselect ? 'mouseleave' : 'mouseenter'
+
+        this.boardPositions[this.selectedColumn][this.selectedRow]
+            .dispatchEvent(new Event(eventName));
     }
 }
 
