bb9dd2bbfd583ddf1ac58df1d7055b1c4b6f49a7
Skapeven <skapeven@gmail.com>
2018-12-29 21:30:11 -0300

    crea juego con eventos globales, por ahora solo teclado, graficas ASCII, algo funciona mal en el addEventListener ya que despues de escuchar una sola vez se tranca.

--- Desktop/Amonite/Battleship/battleship2.ts
+++ Desktop/Amonite/Battleship/battleship2.ts
@@ -1,79 +1,3 @@
-// let width = 8;
-// let height = 6;
-// let board:string = '';
-
-// for(let i: number = 0; i < height; i++) {
-
-//     for(let j: number = 0; j < width; j++) {
-//         board += " O";
-//     }
-
-//     board += "<br />";
-// }
-
-// document.write(board);
-// ========================================================
-// let s:string[] = [];
-// s.push("Xdd");
-// s.push("asd");
-// for (let e of s) {
-//     document.write(e);
-// }
-// ========================================================
-// let width = 8;
-// let height = 6;
-// let board:string[][] = [];
-
-// for (let i = 0; i < height; i++) {
-//     board.push([]);
-// }
-
-// for (let i:number = 0; i < height; i++) {
-    
-//     for (let j:number = 0; j < width; j++) {
-//         board[i].push("O ");
-//     }
-// }
-
-// for (let i:number = 0; i < height; i++) {
-
-//     for (let j:number = 0; j < width; j++) {
-//         document.write(board[i][j]);
-//     }
-//     document.write("<br />");
-// }
-// ========================================================
-// function make_board(rows:number, columns:number) {
-//     let board:string[][] = [];
-
-//     for (let i = 0; i < rows; i++) {
-//         board.push([]);
-//     }
-    
-//     for (let i = 0; i < rows; i++) {
-        
-//         for (let j:number = 0; j < columns; j++) {
-//             board[i].push("O ");
-//         }
-//     }
-
-//     return board;
-// }
-
-// function print_board(board:string[][]) {
-//     for (let r of board) {
-
-//         for (let c of r) {
-//             document.write(c);
-//         }
-
-//         document.write("<br />");
-//     }
-// }
-
-// let b = make_board(5, 8);
-// print_board(b);
-// ========================================================
 function randomIntFromInterval(min,max) {// min and max included
     return Math.floor(Math.random()*(max-min+1)+min);
 }
@@ -85,12 +9,18 @@ class Board {
     ship_y:number;
     board:string[][];
     shipfloats:boolean;
+    prevTar:string;
+    prevTarPos_x:number;
+    prevTarPos_y:number;
 
     constructor(rows, columns) {
         this.rows = rows;
         this.columns = columns;
         this.ship_x = randomIntFromInterval(0, columns - 1);
         this.ship_y = randomIntFromInterval(0, rows - 1);
+        this.prevTar = 'O ';
+        this.prevTarPos_x = 0;
+        this.prevTarPos_y = 0;
     }
 
     make_board() {
@@ -124,32 +54,61 @@ class Board {
 
     bomb_position(x:number, y:number) {
         this.board[x-1][y-1] = "X "
-        document.write("<br />");
 
         if (x === this.ship_x && y === this.ship_y) {
-            document.write("You sunk the ship!")
+            document.write("You sunk the ship!");
             this.shipfloats = false;
         }
     }
+
+    target(x:number, y:number) {
+        this.board[this.prevTarPos_y][this.prevTarPos_x] = this.prevTar;
+        this.prevTar = this.board[y-1][x-1];
+        this.prevTarPos_x = x - 1;
+        this.prevTarPos_y = y - 1; 
+        this.board[y-1][x-1] = "A ";
+    }
 }
 
-var canvas: HTMLCanvasElement;
-var ctx: CanvasRenderingContext2D;
+function play(key) {
+    document.write(key);
+    let x;
+    let y;
 
-function gameLoop() {
-    requestAnimationFrame(gameLoop);
-    ctx.fillStyle = "black";
-    ctx.fillRect(0, 0, 200, 300);
+    if (key === 37) { // LEFT
+        document.write("left");
+        x = last_x - 1;
+        y = last_y;
+    } else if (key === 38) { // UP
+        document.write("up");
+        x = last_x;
+        y = last_y - 1;
+    } else if (key === 39) { // RIGHT
+        document.write("right");
+        x = last_x + 1;
+        y = last_y;
+    } else if (key === 40) { // DOWN
+        document.write("down");
+        x = last_x;
+        y = last_y + 1;
+    } else if (key === 13) { // ENTER
+        document.write("enter");
+        board.bomb_position(Number(x),Number(y)); 
+    }
+
+    document.write("<br />");
+
+    last_x = x;
+    last_y = y;
+    let prevChar = board.target(x, y);
+    document.body.innerHTML = '';
+    board.print_board();
 }
 
 let board = new Board(5, 8);
 board.make_board();
 let shipfloats = true;
 board.print_board();
-
-while (board.shipfloats) {
-    let x = window.prompt("Ingresa coordenada \"x\": ");
-    let y = window.prompt("Ingresa coordenada \"y\": ");
-    board.bomb_position(Number(x),Number(y)); 
-    board.print_board();
-}
+let last_x = 1;
+let last_y = 1;
+document.addEventListener('keypress', e => play(e.keyCode));
